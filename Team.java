164dc0b6587bf6e69b581c169c618a903504c66a
jpopo237 <jspores@wgu.edu>
2024-01-22 08:51:39 -0800

    Adds baseball sim to github.

---
+++ Team.java
@@ -0,0 +1,19 @@
+public class Team {
+    private String team;
+
+    //Constructor
+    public Team(String team) { 
+        this.team = team;
+    }
+
+
+    //Getter
+    public String getTeam() {
+        return team;
+    }
+    //Setter
+    public void setTeam(String team) {
+        this.team = team;
+    }
+}
+
